225c14ee2b12bf39b1e196a062dc1b6921e628ab
Ryan Charles <ryancharles040808@gmail.com>
2023-07-31 11:13:10 -0300

    Criado o arquivo variavel.pv

---
+++ variavel.py
@@ -0,0 +1,11 @@
+# input é uma função que permite perguntar algo ao úsuario
+nome = input("Qual é o seu nome?")
+
+'''
+Bloco
+de
+Comentário 
+Permite que várias linhas se torne um comentário
+'''
+
+print("Olá,", nome, ". Tudo bem com você?")
