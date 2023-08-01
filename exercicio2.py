3f31b22851aacff8f888370335ed040a26833ddd
Ryan Charles <ryancharles040808@gmail.com>
2023-08-01 10:39:40 -0300

    acrecentado o int

--- exercicio2.py
+++ exercicio2.py
@@ -2,9 +2,9 @@
 Fazer um programa que pergunte um número inteiro e apresente ao usuário o antecessor e o sucessor do número informativo.
 '''
 
-num = (input("Informe um número inteiro: "))
+num = int(input("Informe um número inteiro: "))
 
 ant = num - 1
-suc =num + 1
+suc = num + 1
 
 print("O antecessor é", ant, "e o sucessor é", suc)
